d9f11db753b56ba0b984b21d2638fcead21559b8
Daisy Ramirez <daisy_r_s@hotmail.com>
2020-04-15 23:26:18 -0600

    Tipos de datos

---
+++ test02.js
@@ -0,0 +1,28 @@
+var tipoString1 = 'commillas Simples'
+console.log(tipoString1)
+var tipoString2 = "commillas Dobles"
+console.log(tipoString2);
+var tipoEntero = 25
+console.log(tipoEntero);
+var tipoFlotante = 432.453
+console.log(tipoFlotante);
+var tipoBooleano1 = true
+console.log(tipoBooleano1);
+var tipoBooleano0 = false
+console.log(tipoBooleano0);
+var tipoNulo = null
+console.log(tipoNulo);
+var tipoIndefinido = undefined
+console.log(tipoIndefinido);
+
+console.log(typeof tipoString1)
+console.log(typeof tipoString2)
+console.log(typeof tipoEntero)
+console.log(typeof tipoFlotante)
+console.log(typeof tipoBooleano1)
+console.log(typeof tipoBooleano0)
+console.log(typeof tipoNulo)
+console.log(typeof tipoIndefinido)
+
+const PI = Math.PI
+console.log(PI)
