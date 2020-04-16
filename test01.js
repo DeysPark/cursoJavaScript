0bc5b937b58bee38f461cfbae594d0134aee7314
Daisy Ramirez <daisy_r_s@hotmail.com>
2020-04-15 23:05:20 -0600

    Tipos de variables

---
+++ test01.js
@@ -0,0 +1,21 @@
+// var variables globales
+// let limitadas al alcance del bloque
+// const variables inmutables
+var saludo = 'Hola Daisy'
+if(saludo) {
+    var saludo = 'Hola yessica'
+    console.log(saludo)
+}
+console.log(saludo)
+
+
+let nombre = 'Daisy'
+if(nombre) {
+    let nombre = 'Yessica'
+    console.log(nombre)
+}
+console.log(nombre)
+
+const PI = 3.1415
+console.log(PI)
+// PI = 50 This cannot be modified
